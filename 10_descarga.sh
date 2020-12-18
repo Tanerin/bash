98c0eeb1bd91dc972b5f133941530c3edda08faa
Tanner <qtulises@gmail.com>
2020-12-18 15:08:11 -0600

    se agrego el gitignore y el programa 10

---
+++ 10_descarga.sh
@@ -0,0 +1,4 @@
+# !bin/bash
+# Programa para ejemplificar el uso de la descarga 
+echo "Descargar informacion de internet"
+wget https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.61/bin/apache-tomcat-8.5.61.zip
