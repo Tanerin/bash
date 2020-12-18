9b6bc74ed6087aebc5ef88f48db4f322d017d9ac
Tanner <qtulises@gmail.com>
2020-12-18 15:19:59 -0600

    Se agrego un poco de formato al reto2.sh

--- Reto2.sh
+++ Reto2.sh
@@ -4,8 +4,10 @@ echo "Favor de introducir sus datos"
 read -p "Ingresa tu nombre: " nombre
 read -p "Ingresa tu apellido: " apellido
 read -n2 -p "Ingresa tu edad: " edad
+echo -e "\n"
 read -p "Ingresa tu direccion: " direccion
 read -n10 -p "Ingresa tu telefono(10 digitos):" telefono
+echo -e "\n"
 echo "Tu completo es: $nombre $apellido"
 echo "Tu edad es: $edad"
 echo "Tu direccion es: $direccion"
